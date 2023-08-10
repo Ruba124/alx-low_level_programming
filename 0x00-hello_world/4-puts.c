7097420bc9e9915dda81b906e996946e13d63ca8
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-10 12:42:30 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * using puts function
+ * prints programming is like building ...
+ * returns 0
+ */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle \n");
+puts(""Programming is like building a multilingual puzzle \n");
     return (0);
 }
