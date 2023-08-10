33eff08e39d450c2d9ddd38d43dcbcdd98035af6
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-10 13:24:53 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * using puts function
- * prints programming is like building ...
- * returns 0
+ * main - prints programming , h
+ * Return: always 0
  */
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle \n");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
 }
