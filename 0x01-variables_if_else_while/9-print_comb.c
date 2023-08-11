134cf428dcceccdf50b11aa66a7a101b81bf1f4d
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-11 12:59:25 -0700

    l

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,13 @@ int main(void)
 	int a;
 
 	for (a = 48; a <= 57; a++)
-	{putchar(32);
-	putchar(a);
-	putchar(','); }
+	{putchar(a);
+	if (a == 57)
+{
+	continue;
+}
+	putchar(',');
+	putchar(32);}
 	putchar('\n');
 	return (0);
 }
