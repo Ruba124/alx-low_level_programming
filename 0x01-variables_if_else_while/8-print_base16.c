30254fdd2c17fc994f399c6abf9ba8743f74917d
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-11 14:59:37 -0700

    hhj

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry point .prints 012345xyz
+ * Return: always 0
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 48; t <= 107; t++)
+{
+	if ((t > 57) && (t < 97))
+{
+	continue;
+}
+	putchar (t);
+}
+	putchar ('\n');
+	return (0);
+}
