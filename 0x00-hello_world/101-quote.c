9ed2fddf1c4e7f86adde7e591fd38f3e9ccd3ae4
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-12 08:48:39 -0700

    djs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Return:always1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
+}
