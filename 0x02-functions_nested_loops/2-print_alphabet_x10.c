96984ad996e3b66e6081e9026a8e4ff4e35a7f4e
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-15 04:52:02 -0700

    k

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -prints abcd 10 times
+ * Return:always 0
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	for (a = 0; a <= 10; a++)
+{
+	for (c = 97; c <= 122; c++)
+{
+	_putchar (c);
+}
+	_putchar ('\n');
+}
+	putchar ('\n');
+}
