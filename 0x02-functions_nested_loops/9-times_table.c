e307c8f8a68d2964e39d32aae3b825344e8f9ceb
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-15 07:44:21 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,12 @@ void times_table(void)
 {
 	int a, b;
 
-	for(a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 {
-	for(b = 0; b <= 9; b++)
+	for (b = 0; b <= 9; b++)
 {
-	printf("%d",a*b);
+	printf("%d",a * b);
 }
 	putchar('\n');
+}
 }
