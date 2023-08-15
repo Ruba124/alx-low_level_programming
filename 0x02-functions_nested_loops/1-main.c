bf0ede23472b8ada2cb7f4f95326af49274dc0fe
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-15 03:12:54 -0700

    ll

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+void print_alphabet(void);
+/**
+ * main -entry point
+ * Return: always 0
+ */
+int main(void)
+{	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+/**
+ * print_alphabet -prints abcd..z
+ * Return:no return value
+ */
+void print_alphabet(void)
+{
+	int a, b;
+
+	for (a = 96; a <= 122; a++)
+{
+	putchar (a);
+}
+	putchar ('\n');
+}
