0772347f89c4076fcb14eda04c3b9921bc96e326
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-15 07:38:32 -0700

    ;

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table -prints 9 tables
+ * Return: no return
+ */
+void times_table(void)
+{
+	int a;
+
+	for(a = 0; a <= 9; a++)
+{
+	for(b = 0; b <= 9; b++)
+{
+	printf("%d",a*b);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _putchar(char a);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _abs(int);
+void times_table(void);
 #endif
