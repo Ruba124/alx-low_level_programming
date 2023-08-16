ac0eb1deaf219f03a504ed8be70e710493e56b1e
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-16 09:27:45 -0700

    kk

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - from the name
+ * @l:parameter
+ * Return:always 0
+ */
+int print_last_digit(int l)
+{
+	int a = l;
+
+	return (a % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,6 @@ void times_table(void);
 void print_times_table(int n);
 void print_to_98(int n);
 void jack_bauer(void);
+int add(int, int);
+int print_last_digit(int);
 #endif
