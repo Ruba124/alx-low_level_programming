a03847864233d361bf324c446354323dc6ab74ae
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-16 14:07:23 -0700

    kkkjjhhkk

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_times_table - from the name
+ * @n:number
+ * Return:no
+ */
+void print_times_table(int n)
+{
+	int a = n;
+
+	if ((a >= 0) && (a <= 15))
+{
+	for (a = 0; a <= n; a++)
+{
+	for (b = 0; b <= n; b++)
+{
+	printf("%3d," a*b);
+}
+}
+}
+}
