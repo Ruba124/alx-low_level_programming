79abdae64a9eaefd4fd4a279472053b2026c876d
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 16:58:08 -0700

    lll

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return:always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			if (a % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Fizz");
+			}
+		}
+		if (a % 5 == 0 && a % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		if (a % 3 != 0 && a % 5 != 0 && a % 15 != 0)
+		{
+			printf("%d", a);
+		}
+		if (a == 100)
+		{	continue; }
+		putchar (32);
+	}
+	return (0);
+}
