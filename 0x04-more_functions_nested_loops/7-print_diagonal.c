7c9b2cb29c1082012c58813e6a5b10e654704b0f
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 14:57:37 -0700

    kk;

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal-function
+ * @n:length
+ * Return:no
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		if (n >= 0)
+		{
+			_putchar ('\n');
+			break;
+		}
+		for (b = 0; b <= a; b++)
+		{
+			_putchar (32);
+		}
+		_putchar (92);
+		_putchar ('\n');
+	}
+}
+
+
