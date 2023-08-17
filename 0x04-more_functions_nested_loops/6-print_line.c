6478fdd23d3937e4ba587d72e46d19c40d5bb960
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 14:21:20 -0700

    kkk

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line-from the name
+ * @n:the length
+ * Return:no
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		if (n >= 0)
+		{
+			_putchar ('\n');
+			break;
+		}
+		else
+			_putchar ('_');
+	}
+	_putchar ('\n');
+}
