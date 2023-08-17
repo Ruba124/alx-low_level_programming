466e477c445a215edcd81f9fc23a347ed9f4c575
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 14:08:54 -0700

    kkk

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,20 @@
  */
 void more_numbers(void)
 {
-	int a, b;
+	int a, b, c;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
-			printf("%d", b);
+		{
+			c = b;
+			if (c > 9)
+			{
+				_putchar (49);
+				c = b % 10;
+			}
+				_putchar (c + 48);
+		}
 	_putchar ('\n');
 	}
-	_putchar ('\n');
 }
