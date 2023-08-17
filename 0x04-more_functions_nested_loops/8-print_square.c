5a383a12602bb9d8dfe14542a8eee26d1e2d28b2
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 15:37:50 -0700

    lkkk

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square-prints square
+ * @size:size
+ * Return:no
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 1; a <= size; a++)
+	{
+		if (size <= 0)
+		{	break; }
+		for (b = 1; b < size; b++)
+		{
+			_putchar (35);
+		}
+		_putchar ('\n');
+	}
+	if (size <= 0)
+	{	_putchar ('\n'); }
+}
