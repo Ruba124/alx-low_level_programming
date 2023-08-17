fdced3f92c89d6dae5c6cffcdc8594e45c4ebec0
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-17 12:27:44 -0700

    ldld

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,13 @@
  * more_numbers-function
  * Return: no
  */
-oid more_numbers(void)
+void more_numbers(void)
 {
 	int a, b;
 
 	for (a = 0; a < 10; a++)
-	{	for (b = 0; b <= 14; b++)
+	{
+		for (b = 0; b <= 14; b++)
 			printf("%d", b);
 	_putchar ('\n');
 	}
