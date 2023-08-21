809223af124329b1fa7c5c0d3a21a8449816a916
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-21 15:31:58 -0700

    slsl

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _ato - function
+ * @s:parameter
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int m = 1;
+
+	do {
+		if (*s =='-')
+			m *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n * m);
+}
