9652845c09b0cb3f1f059ad9991be07bbf48224e
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-21 10:50:50 -0700

    lsls

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * @str:para
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int n = 0;
+	int s = 0;
+	int m, j;
+
+	while (str[l] != '\0')
+	l++;
+
+	if (l % 2 == 0)
+	{
+		m = l / 2;
+		while (str[n] != '\0')
+		{
+			if (n < m)
+			{	continue; }
+			_putchar (str[l]);
+			n++;
+		}
+		_putchar ('\n');
+	}
+	else
+	{
+		j = (l - 1) / 2;
+		while (str[s] != '\0')
+		{
+			if (s < j)
+			{	continue; }
+			_putchar (str[s]);
+			s++;
+		}
+		_putchar ('\n');
+	}
+}
