11259149df9071ea967b4030bbcf3af1cd2c6aee
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-23 14:39:38 -0700

    kkkj

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function
+ * @a:int
+ * @n:int2
+ */
+void reverse_array(int *a, int n)
+{
+	int o = n - 1;
+
+	while (o >= 0)
+	{
+		if (o != 0)
+		{ printf(", "); }
+		printf("%d", a[o]);
+		o--;
+	}
+}
