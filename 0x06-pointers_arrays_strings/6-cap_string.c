df45afff4cb69b641a238fdfbf86234f5c0b829f
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-25 02:26:31 -0700

    ll

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,53 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * dele - function
+ * @m:char
+ * Return: 0-
+ */
+int dele(char m)
+{
+	int f;
+	char d[] = " \t\n,;.!?\"(){}";
+
+	for (f = 0; f < 12; f++)
+		if (m == d[f])
+			return (1);
+		return (0);
+}
+/**
+ * low - fun
+ * @w:int
+ * Return:0 - 1
+ */
+
+int low(char w)
+{
+	return (w >= 97 && w <= 122);
+}
 /**
  * cap_string - function
  * @s:string
  * Return:string
  */
+
 char *cap_string(char *s)
 {
-	char *c = s;
-	int m = 0;
+	char *b = s;
+	int fl = 1;
 
 	while (*s)
+{
+	if (dele(*s))
+	{	fl = 1; }
+	else if (low(*s) && fl)
 	{
-		if (*s == 32 || *s == '\n' || *s == '\t' || *s == 40)
-		{
-			s[m + 1] -= 32;
-		}
-		m++;
-		s++;
+		*s -= 32;
+		fl = 0;
 	}
-	
+	else
+		fl = 0;
+	s++;
+}
+	return (b);
+}
