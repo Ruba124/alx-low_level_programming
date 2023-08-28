bcea10ffbd44ea5669a5aa61d3a5e203082f1eaf
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-28 11:28:24 -0700

    lllk

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function
+ * @dest:string1
+ * @src:string2
+ * @n:number
+ * Return:string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
