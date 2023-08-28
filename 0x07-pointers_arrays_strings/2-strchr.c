30fdae57c1ad4e3bb79a5c01e48d09d5919701de
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-28 11:57:07 -0700

    kkk

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - function
+ * @s:string
+ * @c:char
+ * Return:pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int g;
+
+	for (g = 0; s[g] != '\0'; g++)
+	{
+		if (s[g] == c)
+		{
+			return (s + g);
+		}
+	}
+	return ('\0');
+}
