01ca92546580c2e39506a925a332c836ca3ef17b
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-29 13:55:30 -0700

    lsl

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main-prints fibo
  * Return:0
  */
+void main(void)
+{
+	int c;
+	int f1 = 0;
+	int f2 = 1;
+	int sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = f1 + f2;
+		printf("%d", sum);
+		f1 = f2;
+		f2 = sum;
+		if (c == 49)
+		{ _putchar('\n'); }
+		else
+		{ printf(", "); }
+	}
+}
 
