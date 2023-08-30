6197cb92ed1cc8cc397696efcd5c9fcfe1ca46cd
Ruba124 <r.m.d.abkam@gmail.com>
2023-08-30 06:07:09 -0700

    lo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,25 +2,27 @@
 /**
  * _sqrt_recursion - function
  * @n:int
+ * @v:int2
  * Return:sq
  */
-int _sq(int n, int v);
-/**
- * _sq - function
- * @v:value
- * Return:int
- */
+int sq(int n, int v);
 
 int _sqrt_recursion(int n)
 { return (_sq(n, 1)); }
+/**
+ * sq - function
+ * @v:value
+ * @n:int2
+ * Return:sq
+ */
 
-int _sq(int n, int v)
+int sq(int n, int v)
 {
 	if ((v * v) == n)
 	{	return (v); }
 	else if (v * v < n)
 	{	return (_sq(n, v + 1)); }
-	else 
+	else
 		return (-1);
 }
 
