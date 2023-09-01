384ea81e6bf7f66a70c54caac2bcc60d1e4bc94a
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-01 10:17:59 -0700

    k

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc:int
+ * @argv:array
+ * Return:0
+ */
+int main(int argc, char** argv)
+{
+	printf("%s", argv[0]);
+	printf("\n");
+	(void)argc;
+	return (0);
+}
