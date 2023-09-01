5268ba20693fb0da982691633a42e7143ddfb607
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-01 14:28:10 -0700

    ll

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
  * _atoi - function
- * @a:int
+ * @s:int
  * Return:int
  */
 int _atoi(char *s);
 int _atoi(char *s)
 {
-        unsigned int n = 0;
-        int m = 1;
+	unsigned int n = 0;
+	int m = 1;
 
-        do {
-                if (*s == '-')
-                        m *= -1;
-                else if (*s >= '0' && *s <= '9')
-                        n = (n * 10) + (*s - '0');
-                else if (n > 0)
-                        break;
-        } while (*s++);
-        return (n * m);
+	do {
+		if (*s == '-')
+			m *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n * m);
 }
 /**
  * main - function
