f8f6b7982e2c443b0d9e545d00c8ce952453b04f
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-01 11:33:53 -0700

    jj'

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function 
+ * @argc:int 
+ * @argv:array
+ * Return:0
+ */
+int main(int argc, char **argv)
+{
+	int t;
+
+	for (t = 0; t < argc; t++)
+	{
+		printf("%s", argv[t]);
+		printf("\n");
+	}
+	return (0);
+}
