2ae9b2a2d2e9239293a5a111c1d44b5460b933e9
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-01 11:23:28 -0700

    ldld

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc:int
+ * @argv:string
+ * Return:0
+ */
+int main(int argc, char **argv)
+{
+	printf("%d", argc);
+	printf("\n");
+	(void)argv;
+	return (0);
+}
