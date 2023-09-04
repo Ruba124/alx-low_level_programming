69fdefdafad8c81cf55dcf73bbe2ddcca195e6f2
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-04 14:28:14 -0700

    kk

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,23 +9,16 @@
 char *str_concat(char *s1, char *s2)
 {
 	int y = 0;
-	int a = 0;
-	int m = 0;
-	int L = 0;
+	int a = 0, m = 0, L = 0;
 	int S;
 	char *r;
 
 	if (s1 != NULL)
-	{
 		for (; s1[m] != '\0'; m++)
 			;
-	}
 	if (s2 != NULL)
-	{
-	
 		for (; s2[L] != '\0'; L++)
 			;
-	}
 	S = m + L + 1;
 	r = malloc(sizeof(char) * S);
 	if (r == NULL)
