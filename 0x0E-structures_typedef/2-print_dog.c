bb3207b48342a9690c9b1bac3af0e9cc470b4dd5
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-11 10:47:08 -0700

    lsls

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,16 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
-	{	printf("Name: %s", d->name);}
+	{	printf("Name: %s", d->name); }
 	printf("\n");
 	if (d->age == 0)
 		printf("Age: (nil)");
-	else 
-	{	printf("Age: %f", d->age);}
+	else
+	{	printf("Age: %f", d->age); }
 	printf("\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
 	else
-		printf("Owner: %s", d->owner);
+	{	printf("Owner: %s", d->owner); }
+	printf("\n");
 }
