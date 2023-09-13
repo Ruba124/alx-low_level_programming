9906ec986b6c77c926dd849935dc2aa979f4d017
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-13 03:18:11 -0700

    lo

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - function
+ * array:para
+ * size:para2
+ * cmp:siz
+ * Return:int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int l, k;
+
+	if (size <= 0)
+	{	return (-1); }
+	for (l = 0; l < size; l++)
+	{
+		k = cmp(array[l]);
+		if (k != 0)
+		{	return (l); }
+	}
+	return (-1);
+}
