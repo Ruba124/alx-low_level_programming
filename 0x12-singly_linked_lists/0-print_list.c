a08f6d4e23c470a1b29b70095b1d8c90d20a025e
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-21 11:59:21 -0700

    ki

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *  print_list - function
+ *  @h:para
+ *  Return:size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t l;
+
+	for (l = 0; h; l++)
+	{
+		if(!h->str)
+		{	printf("[0] (nil)\n"); }
+		else
+		{	printf("[%d] (%s)\n", h->len, h->str); }
+		h = h->next;
+	}
+	return (l);
+}
