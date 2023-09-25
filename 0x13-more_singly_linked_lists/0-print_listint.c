b95cd5eeb39f41df8463fada6f01b752e38f2e35
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-25 02:26:22 -0700

    ki

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - function
+ * @h:para
+ * Return:size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t a;
+
+	for (a = 0; h; a++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (a);
+}
