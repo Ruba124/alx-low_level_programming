08bb4c8d7c6dffbc8b0de65b8144638cf1981454
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-26 11:15:34 -0700

    kdk

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,17 +12,32 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *h;
+	listint_t *a = *head;
 	unsigned int y;
 
-	if (!*head || !head || !idx || !n)
+	h = malloc((sizeof(listint_t)));
+
+	if (!h || !head )
 	{	return (NULL); }
-	h = *head;
-	for (y = 0;*head && y < idx; y++)
+	h->n = n;
+	h->next = NULL;
+	if (idx == 0)
 	{
-		h = (*head)->next;
+		h->next = *head;
+		*head = h;
+		return (h);
 	}
-	n = (*head)->n;
-	return(h);
+	for (y = 0; a && y < idx; y++)
+	{
+		if (y == idx - 1)
+		{
+			h->next = a->next;
+			a->next = h;
+			return (h);
+		}
+		else
+			a = a->next;
+	}	
+
+	return(NULL);
 }
-
-
