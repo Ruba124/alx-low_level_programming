122e97fb626c0b687a696e5ac942ff0c932c8eee
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-26 17:26:02 -0700

    jjj

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,9 +12,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *ny;
 	listint_t *k = *head;
+
 	if (!head || !*head)
 	{	 return (NULL); }
 	ny = malloc(sizeof(listint_t));
+
 	if (!ny)
 	{	return (NULL); }
 	ny->n = n;
