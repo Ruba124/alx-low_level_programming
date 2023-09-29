3a85128f27a2df560b8e2ed7b13af96684a8119b
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-30 01:37:57 +0300

    lo

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * binary_to_unit - functio
+ * Return:ll
+ * @b:int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int you;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (you = 0; *b;)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		you = you * 2 + (*b++ - '0');
+	}
+	return (you);
+}
+
