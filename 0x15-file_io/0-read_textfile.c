a318969d7156cc3971c13120b39e706110cd49da
Ruba124 <r.m.d.abkam@gmail.com>
2023-10-02 13:43:49 +0300

    ki

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * read_textfile - function
+ * @filename:para
+ * @letters:para
+ * Return:size
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t l, k, o;
+	char *ju;
+	
+	if (!filename || !letters)
+	{	return (0); }
+	ju = malloc(sizeof(char) * letters);
+	l = open(filename, O_RDONLY);
+	k = read(l, ju, letters);
+	o = write(STDOUT_FILENO, ju, k);
+	if (l == -1 || k == -1 || o == -1)
+	{	return (0); }
+	close (l);
+	return (o);
+}
+
+
