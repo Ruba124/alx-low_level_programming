4d86463d5c8dd377dcbd39d1f6e7458054572c85
Ruba124 <r.m.d.abkam@gmail.com>
2023-10-02 17:57:48 +0300

    lo

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while(text_content[all])
+		while (text_content[all])
 		{	all++; }
 	}
 	a = open(filename, O_WRONLY | O_APPEND);
