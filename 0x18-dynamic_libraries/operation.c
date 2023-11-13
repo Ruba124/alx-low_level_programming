7f5b7c2c27f3573ddcd214720339ae5fd5ecec47
Ruba124 <r.m.d.abkam@gmail.com>
2023-11-14 01:34:45 +0200

    llkl

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,34 @@
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+
+/**
+ * add - function
+ * @a:int
+ * @b:int
+ * Return:sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ ** sub - function
+ ** @a:int
+ ** @b:int
+ ** Return:sum
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ ** div - function
+ ** @a:int
+ ** @b:int
+ ** Return:sum
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/operation2.c
@@ -0,0 +1,22 @@
+int mod(int a, int b);
+int mul(int a, int b);
+/**
+ * mul - function
+ * @a:para
+ * @b:para
+ * Return:mod
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ ** mod - function
+ ** @a:para
+ ** @b:para
+ ** Return:mod
+ **/
+int mod(int a, int b)
+{
+	return (a % b);
+}
