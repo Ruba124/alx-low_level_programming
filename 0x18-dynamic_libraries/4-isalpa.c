8172d04979f069c2dea363a9a9652cf55e2a27c4
Ruba124 <r.m.d.abkam@gmail.com>
2023-11-13 19:08:22 +0200

    lkl

---
+++ 0x18-dynamic_libraries/4-isalpa.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - func
+ * @c:para
+ * Return:int
+ */
+
+int _isalpha(int c)
+{
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,4 +1,9 @@
 #include <unistd.h>
+/**
+ * _putchar - function
+ * @c:para
+ * Return:int
+ */
 int _putchar(char c)
 {
 		return (write(1, &c, 1));
